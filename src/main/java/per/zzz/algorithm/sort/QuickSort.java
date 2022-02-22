0cf8db9d25fa1ea7fa17e4ac99c1ffc8137d4c4b
阿忠 <2669918628@qq.com>
2022-02-22 17:39:28 +0800

    first commit

---
+++ src/main/java/per/zzz/algorithm/sort/QuickSort.java
@@ -0,0 +1,37 @@
+package per.zzz.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author 阿忠 2669918628@qq.com
+ * @since 2022/2/14 16:39
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] num = {1, 5, 9, 6, 2, 3, 5, 8, 7, 2};
+        System.out.println(Arrays.toString(qSort(num, 0, num.length - 1)));
+    }
+
+    public static int[] qSort(int[] numbers, int left, int right) {
+        int i = left, j = right;
+        int temp = numbers[i];
+        while (i < j) {
+            while (i < j && temp < numbers[j]) {
+                j--;
+            }
+            while (i < j && temp > numbers[i]) {
+                i++;
+            }
+            if (numbers[i] == numbers[j] && i < j) {
+                i++;
+            } else {
+                int swap = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = swap;
+            }
+        }
+        if(i - left > 1) numbers = qSort(numbers, left, i - 1);
+        if(right - j > 1) numbers = qSort(numbers, j + 1, right);
+        return numbers;
+    }
+}
