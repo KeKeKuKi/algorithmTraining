427b79eae42e3ffe57853c845e824a8be92a97b7
阿忠 <2669918628@qq.com>
2022-04-02 11:00:36 +0800

    dfs

---
+++ src/main/java/per/zzz/algorithm/depthFirst/TargetSum.java
@@ -0,0 +1,32 @@
+package per.zzz.algorithm.depthFirst;
+
+/**
+ * @author 阿忠 2669918628@qq.com
+ * @since 2022/4/2 11:00
+ * 494. 目标和
+ * 给你一个整数数组 nums 和一个整数 target 。
+ *
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ *
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ */
+public class TargetSum {
+    int count = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return count;
+    }
+
+    public void backtrack(int[] nums, int target, int index, int sum) {
+        if (index == nums.length) {
+            if (sum == target) {
+                count++;
+            }
+        } else {
+            backtrack(nums, target, index + 1, sum + nums[index]);
+            backtrack(nums, target, index + 1, sum - nums[index]);
+        }
+    }
+}
